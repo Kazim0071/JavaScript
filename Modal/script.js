9f010642407e617d4714f19871b1809b2ed751bd
Muhammad Qasim <qasim.ameer.dk@gmail.com>
2025-08-27 02:31:44 +0500

    Track Modal as a normal folder instead of submodule

---
+++ Modal/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnShowModal = document.querySelectorAll(".show-modal");
+
+const OpenModal = function(){
+    modal.classList.remove("hidden");
+    overlay.classList.remove('hidden');
+}
+
+const CloseModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+xyz
+
+for ( let i = 0; i < btnShowModal.length; i++){
+    btnShowModal[i].addEventListener('click', OpenModal)
+}
+
+btnCloseModal.addEventListener('click', CloseModal)
+
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    CloseModal();
+  }
+});

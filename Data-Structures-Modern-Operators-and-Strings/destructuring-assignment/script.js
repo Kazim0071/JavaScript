ed1c08dc577a13e371f114df22f5ee4e9c8d0bd6
Muhammad Qasim <qasim.ameer.dk@gmail.com>
2025-08-27 02:43:01 +0500

    Answered all The Assignment Questions and Update the file

--- Data-Structures-Modern-Operators-and-Strings/destructuring-assignment/script.js
+++ Data-Structures-Modern-Operators-and-Strings/destructuring-assignment/script.js
@@ -228,12 +228,12 @@ const books = [
 // console.log(Object.keys(books));
 
 // 1.1 Destructure the books array into two variables called firstBook and secondBook.
-// const [firstBook, secondBook] = books;
-// console.log(Object.keys(firstBook, secondBook));
+const [firstBook, secondBook] = books;
+console.log(Object.keys(firstBook, secondBook));
 
 // 1.2 Log the titles of firstBook and secondBook.
-// const [, ,thirdBook] = books;
-// console.log(thirdBook)
+const [, ,thirdBook] = books;
+console.log(thirdBook)
 
 // 1.3 Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount.
 // In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.

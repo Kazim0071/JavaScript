838505f5e1dba276c6e4a5ac30af9d14caa7b0d2
Muhammad Qasim <qasim.ameer.dk@gmail.com>
2025-09-08 12:27:12 +0500

    Worked on Object Prototypes and Prototypal Inheritance

--- OOP/script.js
+++ OOP/script.js
@@ -21,3 +21,27 @@ console.log(jonas);
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
+
+
+// Prototypes
+Person.prototype
+Person.prototype.calcAge = function(){
+    console.log(2037 - this.birthYear);
+}
+
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));    
+
+//  .prototypeOfLinkedObjects
+Person.prototype.species = "Homo Sapiens";
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));

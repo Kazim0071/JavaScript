bd82b6bfc253ed65d794e517a5d30651a63ade35
Muhammad Qasim <qasim.ameer.dk@gmail.com>
2025-09-15 13:27:34 +0500

    working on the backend development

--- Node/index.js
+++ Node/index.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
+const http = require('http');
 
+////////////////////////////////////////////////////////////////
+////Files
 // Blocking synchronous way
 // const data = fs.readFileSync('./txt/input.txt', 'utf8');
 // console.log(data);
@@ -10,20 +13,30 @@ const fs = require("fs");
 // console.log('File written successfully');
 
 // Non-blocking asynchronous way
-fs.readFile(`./txt/start.txt`, "utf-8", (err, data) => {
-  console.log(data);
+// fs.readFile(`./txt/start.txt`, "utf-8", (err, data) => {
+//   console.log(data);
 
-  fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data1) => {
-    console.log(data1);
+//   fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data1) => {
+//     console.log(data1);
 
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data2) => {
-      console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data2) => {
+//       console.log(data2);
 
-      fs.writeFile(`./txt/final.txt`, `${data1}\n${data2}`, "utf-8", (err) =>
-        console.log(`Your File has been Written`)
-      );
-    });
-  });
-});
+//       fs.writeFile(`./txt/final.txt`, `${data1}\n${data2}`, "utf-8", (err) =>
+//         console.log(`Your File has been Written`)
+//       );
+//     });
+//   });
+// });
 
-console.log("Will read your files");
+// console.log("Will read your files");
+
+////////////////////////////////////////////////////////////////
+////Server
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server')
+})
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening from the server on port 8000')
+})
